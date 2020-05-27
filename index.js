88edd58e62e4bcdee80e60c29d0ead4751f97170
Tatiana Grigorieva <tsg1204@gmail.com>
2020-05-26 21:43:53 -0400

    mongo setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const app = express();
 
