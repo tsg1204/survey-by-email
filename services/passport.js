3a8db0877d2b756620da93b7149be96349dd9a84
Tatiana Grigorieva <tsg1204@gmail.com>
2020-05-27 18:38:27 -0400

    client added

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,14 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    //we have this user
-                    done(null, existingUser);
-                } else {
-                    //create a new user in db
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
-    })
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+            if (existingUser) {
+                return done(null, existingUser);
+            } 
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+        }
+    )
 );
