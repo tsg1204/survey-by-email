de86325ca64435f97691290faa9892511592c07a
Tatiana Grigorieva <tsg1204@gmail.com>
2020-08-16 12:33:35 -0400

    charge tested

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,9 +1,17 @@
 const keys = require('../config/keys');
-//const stripe = ('stripe')(keys.stripeSecretKey);
+const { Stripe } = require('stripe');
+const stripe = Stripe(keys.stripeSecretKey);
 
 module.exports = app => {
-   app.post('/api/stripe', (req, res) => {
-        console.log(req.body);
+   app.post('/api/stripe', async (req, res) => {
+        //console.log(req.body);
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 fro 5 credits',
+            source: req.body.id
+        });
+        console.log(charge);
    });
 
 };
