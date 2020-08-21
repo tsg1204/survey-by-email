3380da517be26aa931a14153250729198792272a
Tatiana Grigorieva <tsg1204@gmail.com>
2020-08-21 10:17:56 -0400

    mailer class added

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,5 +3,42 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-    
+    constructor({ subject, recipients }, content) {
+        super();
+
+        this.from_email = new helper.Email('tsg1204@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+        //using build in function of Mail helper
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        })
+    }
+
+    addClickTracking() {
+        //sendgrids setup wht's required
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
 }
+
+module.exports = Mailer;
