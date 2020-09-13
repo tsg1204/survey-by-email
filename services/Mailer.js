e640822baa170131bdd98950bc064a258eddf6c0
Tatiana Grigorieva <tsg1204@gmail.com>
2020-09-13 09:10:10 -0400

    sendgrid fixed

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,54 +3,53 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-    constructor({ subject, recipients }, content) {
-        super();
+  constructor({ subject, recipients }, content) {
+    super();
 
-        this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('tsg1204@gmail.com');
-        this.subject = subject;
-        this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
-        //using build in function of Mail helper
-        this.addContent(this.body);
-        this.addClickTracking();
-        this.addRecipients();
-    }
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('tsg1204@gmail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
 
-    formatAddresses(recipients) {
-        return recipients.map(({ email }) => {
-            return new helper.Email(email);
-        })
-    }
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
 
-    addClickTracking() {
-        //sendgrids setup wht's required
-        const trackingSettings = new helper.TrackingSettings();
-        const clickTracking = new helper.ClickTracking(true, true);
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 
-        trackingSettings.setClickTracking(clickTracking);
-        this.addTrackingSettings(trackingSettings);
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
 
-    }
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 
-    addRecipients() {
-        const personalize = new helper.Personalization();
-        this.recipients.forEach(recipient => {
-            personalize.addTo(recipient);
-        });
-        this.addPersonalization(personalize);
-    }
+  addRecipients() {
+    const personalize = new helper.Personalization();
 
-    async send() {
-        const request = this.sgApi.emptyRequest({
-            method: 'POST',
-            path: '/v3/mail/send',
-            body: this.toJSON()
-        });
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
 
-        const response = this.sgApi.API(request);
-        return response;
-    }
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
+  }
 }
 
 module.exports = Mailer;
