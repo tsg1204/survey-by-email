b1e462fd8d3b0d49b36a7f78ab2afe41e3038d63
Tatiana Grigorieva <tsg1204@gmail.com>
2020-09-17 19:32:52 -0400

    add db update logic

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,14 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
       res.send('Thanks for voting!');
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
       const p = new Path('/api/surveys/:surveyId/:choice');
 
-      const events = _.chain(req.body)
+      _.chain(req.body)
         .map( (event) => {
           const match = p.test(new URL(event.url).pathname);
           if (match) {
@@ -26,10 +26,25 @@ module.exports = app => {
         })
         .compact()
         .uniqBy( 'email', 'surveyId')
+        .each(event => {
+
+          Survey.updateOne(
+            {
+              _id: event.surveyId,
+              recipients: {
+                $elemMatch: { email: event.email, responded: false}
+              }
+            }, 
+            {
+              $inc: { [event.choice]: 1 },
+              $set: { 'recipients.$.responded': true },
+              lastResponded: new Date(),
+            }
+          ).exec();
+
+        })
         .value();
 
-      //console.log(uniqueEvents);
-      console.log(events);
       res.send({});
     })
 
