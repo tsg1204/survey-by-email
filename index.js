69b2bfa26e17b61760e2ec9f76b9d1591db741ed
Tatiana Grigorieva <tsg1204@gmail.com>
2022-03-30 15:11:32 -0400

    test connection

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Survey');
 require('./services/passport');
+//test
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
